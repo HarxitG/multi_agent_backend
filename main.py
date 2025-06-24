9a486e69e591f88e647500a3ba323a05c9f9d4ba
Harshit Soni <h_soni@mt.iitr.ac.in>
2025-06-24 14:56:35 +0530

    Update main.py for Render deployment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
 from api.handlers import router
-import uvicorn
 
 app = FastAPI()
 app.include_router(router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
